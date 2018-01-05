422c860f359730f622a2acb9056e82a37e5e1bca
Brian Gonzalez <me@briangonzalez.org>
2018-01-05 10:21:15 -0800

    init

---
+++ bar.ts
@@ -0,0 +1,2 @@
+export const hello = () => { console.log('Hello from bar.ts') }
+export const goodbye = () => { console.log('Goodbye from bar.ts') }
---
+++ baz.ts
@@ -0,0 +1,5 @@
+import { goodbye } from './foo'
+
+goodbye()
+
+
