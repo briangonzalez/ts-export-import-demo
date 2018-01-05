d2938d88cae03ad920a103eef49189a02b3047ac
Brian Gonzalez <me@briangonzalez.org>
2018-01-05 10:31:24 -0800

    with default export

--- bar.ts
+++ bar.ts
@@ -1,2 +1,4 @@
-export const hello = () => { console.log('Hello from bar.ts') }
-export const goodbye = () => { console.log('Goodbye from bar.ts') }
+export default {
+  hello: () => { console.log('Hello from bar.ts') },
+  goodbye: () => { console.log('Goodbye from bar.ts') }
+}
